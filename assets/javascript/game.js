2a76e80b75c1a6fea1370f2a8177d34d8e5c6cd4
Jodi Tivis <joditivis@gmail.com>
2019-10-14 12:59:48 -0600

    adding for loop to result function, feeling lost

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,8 +7,29 @@ window.onload = function () {
     var chosenWord;
 
     var showLives = document.getElementById("lives");
-    var showWord = document.getElementById("word")
+    var showWord = document.getElementById("word");
 }
+
+  result = function () {
+      wordHolder = document.getElementById('hold');
+      correct = document.createElement('ul');
+
+      for (var i = 0; i < word.length; i++) {
+          correct.setAttribute('id', 'words');
+          guess = document.createElement ('li');
+          guess.setAttribute('class', 'guess');
+          if (word[i] === " ") {
+              guess.inner.HTML = " ";
+              space = 1;
+          } else {
+              guess.innerHTML = "_";
+          }
+
+          guesses.push(guess);
+          wordHolder.appendChild(correct);
+          correct.appendChild(guess);
+      }
+  }
 
 //Words that will filter throughout the game
 var words = [
@@ -31,9 +52,12 @@ var words = [
 var word = words[Math.floor(Math.random() * words.length)];
 
 // Answers
-var answerArray = [];
-for (var i = 0; i < word.length; i++) {
-    answerArray[i] = "_";
+newFunction();
+var remainingLetters = word.length;
+function newFunction() {
+    var answerArray = [];
+    for (var j = 0; j < word.length; j++) {
+        answerArray[j] = "_";
+    }
 }
 
-var remainingLetters = word.length;

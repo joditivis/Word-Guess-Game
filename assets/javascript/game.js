2335314f0e5bac85e117dd00bf60de41f88d2d95
Jodi Tivis <joditivis@gmail.com>
2019-10-14 15:20:53 -0600

    trial and error

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,27 +16,9 @@
 
     var wins = 0;
     var currentWord = [];
-    var alreadyGuessed = [];
     var lives = 10;
-
-    var directionsText = document.getElementById("directions-text");
-    var playerChoice = document.getElementById("words");
-    var winsText = document.getElementById("wins");
-    var currentWordText = document.getElementById("current-word");
-    var livesText = document.getElementById("lives");
-    var alreadyGuessedText = document.getElementById("already-guessed");
-}
-    document.onkeyup = function(event) {
-
-        var playerGuess = event.key;
+    var alreadyGuessed = [];
 
 
-    directionsText.textContent = "";
-
-    winsText = "Wins: " + wins;
-    currentWordText = "Current word: " + words;
-    livesText = "Remaining guesses: " + lives;
-    alreadyGuessedText.textContent = "Letters already guessed: " + event.key;
-}
 
 

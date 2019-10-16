f0e04124b83bf1efd6216a62cfeb02f9421e1492
Jodi Tivis <joditivis@gmail.com>
2019-10-16 11:23:19 -0600

    better organized javascript sections

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,24 +1,34 @@
-    var words = [
-     "Empire State Building", 
-     "Taxi", 
-     "Broadway", 
-     "China Town", 
-     "Central Park", 
-     "Statue of Liberty",
-     "Coney Island",
-     "Big Apple",
-     "Traffic",
-     "Concrete Jungle",
-     "Times Square",
-     "Brooklyn Bridge",
-     "The Metropolitan Museum",
-]; 
+// GLOBAL VARIABLES
+var words = [
+     "empire state building", 
+     "taxi", 
+     "broadway", 
+     "china town", 
+     "central park", 
+     "statue of liberty",
+     "coney island",
+     "big apple",
+     "traffic",
+     "concrete jungle",
+     "times square",
+     "brooklyn bridge",
+     "the metropolitan museum",
+     "hot dogs",
+     "rockefeller center",
+     "fifth avenue",
+     "grand central station",
+     "wall street"]; 
 
-    var wins = 0;
-    var currentWord = [];
-    var lives = 10;
-    var alreadyGuessed = [];
+var chosenWord = "";
+var lettersInWord = [];
+var numOfBlanks = 0
+var blanksAndGuesses = [];
+var wrongGuesses = [];
+
+var winCount = 0;
+var guesses = 10;
+
+// FUNCTIONS
 
 
-
-
+// MAIN PROCESS

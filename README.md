0f5f441458e1848243b4712f07ab2d4071893d3a
Jodi Tivis <joditivis@gmail.com>
2020-01-20 18:00:48 -0700

    updated readme, fixed spacing within JS code

--- README.md
+++ README.md
@@ -2,10 +2,9 @@
 **Link to Game: https://joditivis.github.io/Word-Guess-Game/**
 
 # About:
-University of Denver Coding Boot Camp third week assignment: This assigmnet required us to create a Word Guess Game using JavaScript.
-
+University of Denver Coding Boot Camp third week assignment: Required us to build a Word Guess Game using JavaScript.
+---
 # How to Play:
-```
 - New York City Theme and Category: What are some common places or things New York City is known for?
 
 - Guess the correct word before you run out of guesses to receive a win point for that round!
@@ -14,15 +13,15 @@ University of Denver Coding Boot Camp third week assignment: This assigmnet requ
 
 - Try to get more wins than losses!
 
-- When you see a " - " in the current word, it is representing a space
-
 - Listen to a little tune while you play!
-```
 
+- When you see a " - " in the current word, it is representing a space
+---
 # Tools + Languages Used:
-* HTML
-* CSS
-* JavaScript
+* HTML5 - markup language
+* CSS3 - styling
+* JavaScript - programming language
+
 * Page Colors found on https://coolors.co/3d3522-4a442d-386150-58b09c-caf7e2
 * Song by Taylor Swift found on https://mp3direct.org/
 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,5 @@
 console.log("javascripts loaded") // makes sure javascript is loading from html
+
 // GLOBAL VARIABLES
 //words that will rotate throughout the game for the player
 var words = [
@@ -21,17 +22,21 @@ var words = [
      "grand-central-station",
      "wall-street",
      "where-dreams-are-made-of"]; 
+
 //variables that will be used later for functions
 var chosenWord = "";  //chosen word from array
 var lettersInWord = []; //how many letters are in the word
 var blankLetters = 0;  //how many blanks we need to display the word
 var blanksAndRightGuesses = [];  //letters still remaining to be guessed (blanks) and correct letters guessed (revealed)
 var space ;
+
 //variables used for the start of the game settings and will be used later for if and else 
 var winCount = 0;  //keeps track of how many times player has won
 var guessesLeft = 10;  //keeps track of the number of guesses the player has left
 var losses = 0;  //keeps track of how many times the player lost
 var wrongGuesses = [];  //letters stored under letters already guessed
+
+
 //________________________________________________________
 // FUNCTIONS to generate the arrays
 //chooses a random word from words array
@@ -47,6 +52,7 @@ function gameStart() { //start of game function
 
 //populate blanksAndRightGuesses with correct number of blanks
     for (var i = 0; i < blankLetters; i++) { //takes the lettersInWord.length and determines the number of blanks needed
+        
         //blanksAndRightGuesses.push("_"); //pushes the _ to show in place of each blank letter
         if (lettersInWord[i] === "-") {
             blanksAndRightGuesses.push("-");
@@ -55,17 +61,20 @@ function gameStart() { //start of game function
           blanksAndRightGuesses.push("_");
       }
     }   
+
 //connecting html aspects
     document.getElementById("currentWord").innerHTML = blanksAndRightGuesses.join(" "); //.join makes the blank letters appear without the commas like they are in the console
     document.getElementById("lives").innerHTML = guessesLeft;
     document.getElementById("wins").innerHTML = winCount;
     document.getElementById("losses").innerHTML = losses;
+
 //logging to make sure everything is showing up and working correctly
     console.log(chosenWord);
     console.log(lettersInWord);
     console.log(blankLetters);
     console.log(blanksAndRightGuesses);
 }
+
 function checkLetters(letter) {     
     //check if letter exists in word at all
     var isletterInWord = false; //variable for below statment for (isletterInWord)
@@ -74,6 +83,7 @@ function checkLetters(letter) {
            isletterInWord = true; //if the letter is in the word
        }
    } 
+
    //check where in the word the letter exists then populate blanksAndRightGuesses
      if (isletterInWord) { //if letter isn't in the word
         for(var i = 0; i < blankLetters; i++) {
@@ -82,22 +92,26 @@ function checkLetters(letter) {
           }
       }
   }  
+
   //if letter wasn't found
   else {
       wrongGuesses.push(letter); //pushes wrong guessed letter to letters already guessed
       guessesLeft--; //subtracts from the amount of guesses player has left
   }
+
  //testing and debugging
   console.log(blanksAndRightGuesses);
   console.log(isletterInWord);
   console.log(letter);
 }
+
 function roundComplete() { 
     console.log("Wins: " + winCount + " Losses: " + losses + " Guesses left:" + guessesLeft);
     //make sure each round updates HTML to reflect the most recent information
     document.getElementById("lives").innerHTML = guessesLeft;
     document.getElementById("currentWord").innerHTML = blanksAndRightGuesses.join(" ");
     document.getElementById("letters-guessed").innerHTML = wrongGuesses.join(", ");   
+    
     //checks if user won
     if (lettersInWord.toString() == blanksAndRightGuesses.toString()) {
         winCount++; 
@@ -113,25 +127,30 @@ function roundComplete() {
         gameStart();
     }
 }
+
 // MAIN PROCESS
 // initiates the code the first time
 gameStart();
+
 // register key clicks
 document.onkeyup = function(event) {
      var letterGuessed = String.fromCharCode(event.keyCode).toLowerCase();
      checkLetters(letterGuessed)
      roundComplete()
      console.log(letterGuessed);
+
      //doesn't allow user to guess same letter twice
      document.onkeyup = function (event) {
         var letterGuessed;
+
        // captures keypress, eliminating repeat letters
        if (event.keyCode >= 65 && event.keyCode <= 90) {
          letterGuessed = event.key;
 
          if(wrongGuesses.indexOf(letterGuessed)!==-1){
             return;
-        }         
+        }  
+
       // Runs code to check for correct guesses
       checkLetters(letterGuessed);   
       // Runs code that ends each round
